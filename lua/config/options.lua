4f6b493007d95a004874d7cec6d186dbd9a1e17a
Renê Schneider <renews@gmail.com>
2024-08-26 22:17:41 +0200

    add stuff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.splitkeep = "cursor" -- default: screen, keep cursor position when splitting windows
+vim.opt.shell = "fish" -- default: sh, set shell to fish
+vim.opt.backspace = { "start", "eol", "indent" } -- default: "", allow backspacing over everything in insert mode
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*", "*/_build/*" }) -- Ignore this folders always
