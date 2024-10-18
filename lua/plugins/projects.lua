06375b73acc7e9c00617eb2e40d42b99d507d6d9
Renê Schneider <renews@gmail.com>
2024-10-18 14:51:18 +0200

    updates

--- lua/plugins/projects.lua
+++ lua/plugins/projects.lua
@@ -1,7 +1,7 @@
 return {
   "ahmedkhalf/project.nvim",
   opts = {
-    manual_mode = false,
+    manual_mode = true,
     patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "mix.exs" },
   },
 }
