37a561568d37413dddb769e7e12b860a2cfe18c8
Renê Schneider <renews@gmail.com>
2024-11-11 15:41:13 +0100

    updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 local map = vim.keymap.set
 
 -- buffers
-map("n", "<leader>bc", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
-map("n", "<C-q>", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
-map("n", "<leader>bC", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
+map("n", "<C-q>", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,13 +3,40 @@ return {
   version = "*",
   lazy = true,
   opts = {
-    current_line_blame = true,
+    signs = {
+      add = { text = "┃" },
+      change = { text = "┃" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┆" },
+    },
+    -- signs_staged = {
+    --   add = { text = "┃" },
+    --   change = { text = "┃" },
+    --   delete = { text = "_" },
+    --   topdelete = { text = "‾" },
+    --   changedelete = { text = "~" },
+    --   untracked = { text = "┆" },
+    -- },
+    signs_staged_enable = true,
+    signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+    numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+    word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    watch_gitdir = {
+      follow_files = true,
+    },
+    auto_attach = true,
+    attach_to_untracked = false,
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
       virt_text = true,
       virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-      delay = 300,
+      delay = 250,
       ignore_whitespace = false,
       virt_text_priority = 100,
+      use_focus = true,
     },
   },
 }
