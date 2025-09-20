7e644e2c0b4e8b2ab4baf919d610c1bdf892dc82
Renê Schneider <53726+renews@users.noreply.github.com>
2025-09-20 00:21:54 -0300

    remove already ensured by default

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -6,14 +6,9 @@ return {
     opts = function(_, opts)
       -- add tsx and treesitter
       vim.list_extend(opts.ensure_installed, {
-        "lua",
         "elixir",
         "heex",
         "ruby",
-        "yaml",
-        "xml",
-        "regex",
-        "typescript",
       })
     end,
   },
