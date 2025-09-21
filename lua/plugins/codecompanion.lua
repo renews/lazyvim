9742abd66b86efec8b90296cf8046bb5af5b6bc2
Renê Schneider <53726+renews@users.noreply.github.com>
2025-09-21 10:57:13 -0300

    add mchub as codecompanion dependency

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -2,8 +2,20 @@ return {
   {
     "olimorris/codecompanion.nvim",
     dependencies = {
+      "ravitemer/mcphub.nvim",
       "nvim-lua/plenary.nvim",
     },
-    opts = {},
+    opts = {
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
+          },
+        },
+      },
+    },
   },
 }
