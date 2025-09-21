abbf5b129d813b89688196c39795d83f16298769
Renê Schneider <53726+renews@users.noreply.github.com>
2025-09-20 21:19:26 -0300

    lsp config for frontend stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,48 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false }, -- Toggle when needed
+      servers = {
+        -- TypeScript/JavaScript powerhouse
+        tsserver = {
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(".git")(...)
+          end,
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "literal",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+              },
+            },
+          },
+        },
+
+        -- CSS intellisense
+        cssls = {},
+
+        -- Tailwind CSS support
+        tailwindcss = {
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(".git")(...)
+          end,
+        },
+
+        -- HTML support
+        html = {},
+      },
+    },
     config = function()
       vim.lsp.config("expert", {
         cmd = { "expert" },
