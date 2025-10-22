dc4dd2a6fcdc5a3b4def44314872070fe249ebd7
Renê Schneider <53726+renews@users.noreply.github.com>
2025-10-21 21:08:02 -0300

    updates

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -2,9 +2,9 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "cyberdream",
+      -- colorscheme = "cyberdream",
       -- colorscheme = "tokyonight-night",
-      -- colorscheme = "matteblack",
+      colorscheme = "matteblack",
     },
   },
 }
