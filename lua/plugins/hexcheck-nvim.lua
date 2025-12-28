74577a1f747be44c3c0b13c6eccec4d515ef99aa
Renê Schneider <53726+renews@users.noreply.github.com>
2025-12-28 18:29:32 -0300

    up

--- lua/plugins/hexcheck-nvim.lua
+++ lua/plugins/hexcheck-nvim.lua
@@ -5,11 +5,4 @@
 -- }
 return {
   "renews/hexcheck.nvim",
-  cmd = "HexCheck",
-  opts = {
-    highlight_color = "#8ec07c",
-    italic = true,
-    bold = false,
-    message_prefix = "new version available ",
-  },
 }
